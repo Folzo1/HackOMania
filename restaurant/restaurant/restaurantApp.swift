aadcdbdbedee573f13131895e649ab7719c216a7
Folzo <chrisd1231@gmail.com>
2025-02-15 14:25:25 +0800

    test

---
+++ restaurant/restaurant/restaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  restaurantApp.swift
+//  restaurant
+//
+//  Created by Christopher on 15/2/25.
+//
+
+import SwiftUI
+
+@main
+struct restaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
