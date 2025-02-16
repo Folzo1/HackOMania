d953143b297a2f66ad07509e399e03fc963189f4
SYChai <sychai271@gmail.com>
2025-02-16 10:28:04 +0800

    fixed most issues

---
+++ restaurant/restaurant/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  restaurant
+//
+//  Created by Shao Yang Chai on 16/2/25.
+//
+
+import SwiftUI
+
+struct GameScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameScene()
+}
---
+++ restaurant/restaurant/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  restaurant
+//
+//  Created by Shao Yang Chai on 16/2/25.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
